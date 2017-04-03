bc5dd1fcb8fcbcdf44e278e466dc63c7f95c28cc
theminer3746 <bwsminecraft@hotmail.com>
2017-04-03 22:07:35 +0700

    [WIP] Create method to show rank

--- app/Http/Controllers/FamilyController.php
+++ app/Http/Controllers/FamilyController.php
@@ -5,5 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class FamilyController extends Controller{
-    
+    public function showRankPage(Request $request, $rank){
+        
+    }
 }
