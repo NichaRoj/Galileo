d0ece51ff35b5677a29dafc5a82449215c4a98b4
namo <namo@namodev.com>
2017-04-04 05:46:52 +0700

    (A bit of hack) dynamic footer assignments via jQuery. TODO: real double-footer & CSS media queries

--- resources/views/meta_og.blade.php
+++ resources/views/meta_og.blade.php
@@ -1,4 +1,4 @@
 <meta property="og:title" content="ระบบค้นหาสายรหัส TU80" />
-<meta property="og:url" content="https://galileo.theminerdev.com/" />
-<meta property="og:description" content="Crafted with love by @NamoDev @TheMiner3746" />
+<meta property="og:url" content="https://galileo.app.district6.net/" />
+<meta property="og:description" content="ค้นหาสายรหัสได้เร็วขึ้น ด้วยระบบค้นหาสายรหัส" />
 <meta property="og:image" content="/assets/images/bg_med.jpg" />
