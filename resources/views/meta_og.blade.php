01e7dd67d82277fff852d25c4a5c0e28dcfb762a
theminer3746 <bwsminecraft@hotmail.com>
2017-04-04 03:41:32 +0700

    Adjust background

--- resources/views/meta_og.blade.php
+++ resources/views/meta_og.blade.php
@@ -1,4 +1,4 @@
 <meta property="og:title" content="ระบบค้นหาสายรหัส TU80" />
 <meta property="og:url" content="https://galileo.theminerdev.com/" />
 <meta property="og:description" content="Crafted with love by @NamoDev @TheMiner3746" />
-<meta property="og:image" content="/assets/images/bg.jpg" />
+<meta property="og:image" content="/assets/images/bg_med.jpg" />
