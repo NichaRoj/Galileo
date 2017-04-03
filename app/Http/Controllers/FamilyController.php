42c171df9442a19378284ee98e858bfae5f0dd23
theminer3746 <bwsminecraft@hotmail.com>
2017-04-03 23:17:27 +0700

    Make updateStudent method work

--- app/Http/Controllers/FamilyController.php
+++ app/Http/Controllers/FamilyController.php
@@ -65,6 +65,61 @@ class FamilyController extends Controller{
     }
 
     public function updateStudent(Request $request, $id){
+        $student = Student::where('_id', $id)->get();
 
+        if(count($student) === 1){
+            $student = $student[0]; //Flatten the array
+        }else{
+            return response()->json([
+                'msg' => 'Incorrect username or password'
+            ], 401);
+        }
+
+        if(!Hash::check($request->input('password'), $student['password'])){
+            return response()->json([
+                'msg' => 'Incorrect username or password'
+            ], 401);
+        }
+
+        $this->validate($request, [
+            'title' => 'required|string',
+            'fname' => 'required|string',
+            'lname' => 'required|string',
+            'nickname' => 'nullable|string',
+            'generation' => 'required|numeric|max:80',
+            'room' => 'numeric',
+            'rank' => 'required|numeric',
+            'facebook' => 'required_without_all:phone,email,twitter,line,instagram',
+            'email' => 'required_without_all:phone,facebook,twitter,line,instagram',
+            'twitter' => 'required_without_all:phone,email,facebook,line,instagram',
+            'line' => 'required_without_all:phone,email,facebook,twitter,instagram',
+            'instagram' => 'required_without_all:phone,email,facebook,twitter,line',
+            'phone' => 'required_without_all:email,facebook,twitter,line,instagram',
+            'message' => 'nullable',
+        ]);
+
+        Student::where('_id', $id)->update([
+            'title' => (string) $request->input('title'),
+            'fname' => (string) $request->input('fname'),
+            'lname' => (string) $request->input('lname'),
+            'nickname' => (string) $request->input('nickname'),
+            'generation' => (int) $request->input('generation'),
+            'room' => (int) $request->input('room'),
+            'rank' => (int) $request->input('rank'),
+            'contact' => [
+                'facebook' => (string) $request->input('facebook'),
+                'email' => (string) $request->input('email'),
+                'twitter' => (string) $request->input('twitter'),
+                'line' => (string) $request->input('line'),
+                'instagram' => (string) $request->input('instagram'),
+                'phone' => (string) $request->input('phone'),
+            ],
+            'message' => (string) $request->input('message'),
+        ]);
+
+        return response()->json([
+            'msg' => 'Resource updated.',
+            'id' => $id,
+        ], 200);
     }
 }
