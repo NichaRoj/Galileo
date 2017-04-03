b7dc6415364b3671718bc15ad94370c70bc811a6
theminer3746 <bwsminecraft@hotmail.com>
2017-04-03 22:41:17 +0700

    [WIP] Create method to get student's data [WIP] Create method to update student's data Create method to create new student resource

--- app/Http/Controllers/FamilyController.php
+++ app/Http/Controllers/FamilyController.php
@@ -2,10 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use Hash;
+use App\Student;
 use Illuminate\Http\Request;
 
 class FamilyController extends Controller{
     public function showRankPage(Request $request, $rank){
-        
+
+    }
+
+    public function getStudentData(Request $request, $id){
+
+    }
+
+    public function createStudent(Request $request){
+        $this->validate($request, [
+            'title' => 'required|string',
+            'fname' => 'required|string',
+            'lname' => 'required|string',
+            'nickname' => 'nullable|string',
+            'generation' => 'required|numeric|max:80',
+            'room' => 'numeric',
+            'rank' => 'required|numeric',
+            'phone' => 'required_without_all:email,facebook,twitter,line,instagram',
+            'email' => 'required_without_all:phone,facebook,twitter,line,instagram',
+            'facebook' => 'required_without_all:phone,email,twitter,line,instagram',
+            'twitter' => 'required_without_all:phone,email,facebook,line,instagram',
+            'line' => 'required_without_all:phone,email,facebook,twitter,instagram',
+            'instagram' => 'required_without_all:phone,email,facebook,twitter,line',
+            'message' => 'nullable',
+            'password' => 'required',
+            'password_confirm' => 'required|same:password',
+        ]);
+
+        $id = Student::insertGetId([
+            'title' => $request->input('title'),
+            'fname' => $request->input('fname'),
+            'lname' => $request->input('lname'),
+            'nickname' => $request->input('nickname'),
+            'generation' => $request->input('generation'),
+            'room' => $request->input('room'),
+            'rank' => $request->input('rank'),
+            'contact' => [
+                'phone' => $request->input('phone'),
+                'email' => $request->input('email'),
+                'facebook' => $request->input('facebook'),
+                'twitter' => $request->input('twitter'),
+                'line' => $request->input('line'),
+                'instagram' => $request->input('instagram'),
+            ],
+            'message' => $request->input('message'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        return response()->json([
+            'msg' => 'Resource created.',
+            'id' => $id,
+        ], 201);
+    }
+
+    public function updateStudent(Request $request, $id){
+
     }
 }
