d3d968e3a4bf5e5fb8d7ff5e5876322901ed81b2
namo <namo@namodev.com>
2017-04-04 21:17:34 +0700

    Updated meta_og

--- resources/views/meta_og.blade.php
+++ resources/views/meta_og.blade.php
@@ -1,4 +1,4 @@
-<meta property="og:title" content="ระบบค้นหาสายรหัส TU80" />
-<meta property="og:url" content="https://galileo.app.district6.net/" />
-<meta property="og:description" content="ค้นหาสายรหัสได้เร็วขึ้น ด้วยระบบค้นหาสายรหัส" />
-<meta property="og:image" content="/assets/images/bg_med.jpg" />
+        <meta property="og:title" content="ระบบค้นหาสายรหัส TU80" />
+        <meta property="og:url" content="https://galileo.district6.net/" />
+        <meta property="og:description" content="ค้นหาสายรหัสได้เร็วขึ้น ด้วยระบบค้นหาสายรหัส" />
+        <meta property="og:image" content="/assets/images/bg_med.jpg" />
