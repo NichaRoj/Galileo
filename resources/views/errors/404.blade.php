dd42638c35799dd9b754848c1adaa4ffbc22418b
namo <namo@namodev.com>
2017-04-04 20:37:18 +0700

    Added "beautified" 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html>
+    <head>
+        <title>404</title>
+        <meta charset="utf-8" />
+        <style>
+            /* Fonts */
+            @import url("/assets/fonts/fontawesome/fontawesome.css");
+            @import url("/assets/fonts/kanit/kanit.css");
+            *{
+            font-family: "Kanit", "quicksand", sans-serif;
+            cursor: default;
+            user-select: none;
+            -moz-user-select: none;
+            -webkit-user-select: none;
+            -ms-user-select: none;
+            }
+            body{
+            background: #E3E3E3;
+            color: #4A4A4A;
+            }
+            .bigfrown{
+            margin-top: 0.4em;
+            font-size: 15em;
+            color: #4A4A4A;
+            }
+            h1{
+            font-weight: 200;
+            font-size: 7em;
+            margin: 0;
+            }
+            h2{
+            font-weight: 200;
+            font-size: 3em;
+            margin: 0;
+            }
+            .backlink{
+            }
+            a{
+            cursor: pointer;
+            text-decoration: none;
+            color: #4A4A4A;
+            }
+            a:hover{
+            cursor: pointer;
+            text-decoration: none;
+            color: #141414 !important;
+            }
+            a:visited{
+            cursor: pointer;
+            text-decoration: none;
+            color: #4A4A4A;
+            }
+            a:link{
+            cursor: pointer;
+            text-decoration: none;
+            color: #4A4A4A;
+            }
+        </style>
+    </head>
+    <body>
+        <div style="display:flex;justify-content:center;align-items:center;text-align:center">
+            <div class="eMsg">
+                <i class="fa fa-frown-o bigfrown"></i>
+                <br />
+                <h1> 404 </h1>
+                <h2> ไม่พบหน้าที่คุณขอ </h2>
+                <br />
+                <a href="/" target="_self">กลับหน้าหลัก</a>
+            </div>
+        </div>
+    </body>
+</html>
